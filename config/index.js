30ddbd82d6f86414bcb59f4a262a98909175d824
Matheus Bratfisch <matheus@loadsmart.com>
2017-11-28 01:46:50 -0200

    Add fallback

--- config/index.js
+++ config/index.js
@@ -5,6 +5,9 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
+    historyApiFallback: {
+      index: path.resolve(__dirname, '../dist/index.html')
+    },
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
